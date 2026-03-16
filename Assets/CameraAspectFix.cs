053887c7c0a6c274dcfaee5f037f34fd698b4966
moneyyysa <693751821@qq.com>
2026-03-16 21:01:20 +0800

    1
    1

--- Assets/CameraAspectFix.cs
+++ Assets/CameraAspectFix.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 [RequireComponent(typeof(Camera))]
 public class CameraAspectFix : MonoBehaviour
 {
-    // 你设计的原始比例（竖屏 9:16，横屏 16:9）
+
     [Header("设计宽高比（宽/高）")]
     public float targetAspect =1.777f;
 
@@ -12,11 +12,10 @@ public class CameraAspectFix : MonoBehaviour
     void Awake()
     {
         _cam = GetComponent<Camera>();
-        // 强制使用目标比例
+
         _cam.aspect = targetAspect;
     }
 
-    // 可选：运行中也保持（比如旋转屏幕）
     void Update()
     {
         if (_cam.aspect != targetAspect)
